c66cb6af338e7c99592e37cb1a1620d1392b44f4
huhubun <nongzc@outlook.com>
2022-03-30 00:51:54 +0800

    Create the project.

---
+++ src/Blazedis.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazedis.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Blazedis.App/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Blazedis.App.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using MudBlazor.Services;
+
+namespace Blazedis.App
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddBlazorWebView().AddMudServices();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
