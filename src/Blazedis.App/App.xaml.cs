38c5ff5b3723319497960babdbd989540f39709f
huhubun <nongzc@outlook.com>
2022-08-07 12:53:29 +0800

    Initial commit

---
+++ src/Blazedis.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazedis.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Blazedis.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Blazedis.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
